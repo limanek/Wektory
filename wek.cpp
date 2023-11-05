16580e4c463fc0a58be680d6e260ac98d83171d2
Blazej Leman <blazejleman789@gmail.com>
2023-11-05 19:34:51 +0100

    Pętla for po kolekcji

--- wek.cpp
+++ wek.cpp
@@ -4,9 +4,14 @@
 void tabv() {
     std::vector<int> numbers = {1, 2, 3, 4, 5};
     numbers.push_back(6); 
-    std::cout << numbers[5];
-    std::cout << numbers.front();
-    std::cout << numbers.back();
+    std::cout << numbers[5] << std::endl;
+    std::cout << numbers.front() << std::endl;
+    std::cout << numbers.back() << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    for(auto element : numbers) {
+        std::cout << element << std::endl;
+    }
 }
 
 int main() {
